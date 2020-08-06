8db15bb03f2017efcb87bc209ee818bd52fb2d3e
Joshua Draper <josh.drapergame@gmail.com>
2020-08-05 21:33:50 -0600

    First commit: ### Day 1: Get `print8.ls8` running

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -4,8 +4,11 @@
 
 import sys
 from cpu import *
-
 cpu = CPU()
 
-cpu.load()
-cpu.run()
+if len(sys.argv) > 1:
+    cpu.load(sys.argv[1])
+    cpu.run()
+
+else:
+    print("please add a program to run")
